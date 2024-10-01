a7bb4009c7114610db921f0b77e263e71eb44c0e
Jerome Bell <jeromebell@gmail.com>
2024-10-01 16:40:45 -0500

    Unknown Category Bug

--- src/Evanston/Program.cs
+++ src/Evanston/Program.cs
@@ -3,8 +3,8 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
-using System.Text.Json.Serialization;
 using Evanston;
+using Evanston.Util;
 using DirectoryDto = Evanston.DirectoryDto;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -15,7 +15,7 @@ var http = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAd
 builder.Services.AddScoped(_ => http);
 
 var cacheBypass = $"?v={typeof(Program).Assembly.GetName().Version?.ToString(3)}";
-var directory = await http.GetFromJsonAsync<DirectoryDto>($"{http.BaseAddress.AbsoluteUri}/api/v1/directory.json{cacheBypass}", new System.Text.Json.JsonSerializerOptions() { Converters = { new JsonStringEnumConverter() } });
+var directory = await http.GetFromJsonAsync<DirectoryDto>($"{http.BaseAddress.AbsoluteUri}/api/v1/directory.json{cacheBypass}", new System.Text.Json.JsonSerializerOptions() { Converters = { new UnknownEnumConverter() } });
 builder.Services
     .AddSingleton(directory ?? new())
     .AddSingleton(new Mappers());
